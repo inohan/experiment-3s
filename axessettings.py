ed6458fd85dcf6a6739e2ae29a101043fd7409f6
inohaan <sato4inoue2003@gmail.com>
2024-04-16 18:24:00 +0900

    add P1

--- axessettings.py
+++ axessettings.py
@@ -2,7 +2,7 @@ from matplotlib.axes import Axes
 import numpy as np
 
 class AxesSetting():
-    def __init__(self, title="Title", xlabel=None, ylabel=None, xlim=None, ylim=None, xscale="linear", yscale="linear", legend: bool = True, aspect = 'auto'):
+    def __init__(self, title="Title", xlabel=None, ylabel=None, xlim=None, ylim=None, xscale="linear", yscale="linear", legend: bool = True, aspect = 'auto', grid=None):
         self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
@@ -12,6 +12,16 @@ class AxesSetting():
         self.yscale = yscale
         self.legend = legend
         self.aspect = aspect
+        self.grid = {"which": "major", "axis": None}
+        if (type(grid) is tuple or type(grid) is list) and len(grid) == 2:
+            self.grid = {"which": grid[0], "axis": grid[1]}
+        elif type(grid) is dict:
+            if "which" in grid.keys():
+                self.grid["which"] = grid["which"]
+            if "axis" in grid.keys():
+                self.grid["axis"] = grid["axis"]
+        else:
+            self.grid["axis"] = grid
     
     def apply_axes(self, axes: Axes):
         if self.title:
@@ -32,8 +42,10 @@ class AxesSetting():
             axes.legend()
         if self.aspect:
             axes.set_aspect(self.aspect)
+        if self.grid["axis"] is not None:
+            axes.grid(True, self.grid["which"], self.grid["axis"])
     
-    def plot_func(self, axes: Axes, func, xlim = None, samplesize = 200, c="black", label=None, marker=None, alpha=0.2):
+    def plot_func(self, axes: Axes, func, xlim = None, samplesize = 200, **kwargs):
         x = None
         if xlim:
             if self.xscale == "log":
@@ -47,7 +59,7 @@ class AxesSetting():
                 x = np.linspace(self.xlim[0], self.xlim[1], samplesize)
         else:
             raise Exception("Limit not defined.")
-        axes.plot(x, func(x), color=c, marker=marker, label=label, alpha=alpha)
+        axes.plot(x, func(x), **kwargs)
 
 def linear_sim(p1: tuple, p2: tuple, y: float) -> float:
     return (-p1[0] * (y - p2[1]) + p2[0] * (y - p1[1])) / (p2[1] - p1[1])
